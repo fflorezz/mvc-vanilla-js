a72c7ebe19d8e887be65eb213cc3f9535ceda691
fflorezz <felipeflorezart@gmail.com>
2020-03-15 16:17:18 -0500

    Added view's methods

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 class Model {
   constructor() {
     this.todos = [
-      { id: 1, text: "Run a marathon", completed: false },
-      { id: 2, text: "Plant a garden", completed: false }
+      { id: 1, text: "Run a marathon", complete: false },
+      { id: 2, text: "Plant a garden", complete: false }
     ];
   }
 
@@ -64,6 +64,46 @@ class View {
     const element = document.querySelector(selector);
 
     return element;
+  }
+
+  get _todoText() {
+    return this.input.value;
+  }
+
+  _resetInput() {
+    this.input.value = "";
+  }
+
+  displayTodos(todos) {
+    todos = [
+      { id: 1, text: "Run a marathon", complete: false },
+      { id: 2, text: "Plant a garden", complete: true }
+    ];
+    // Delete all nodes
+    while (this.todoList.firstChild) {
+      this.todoList.removeChild(this.todoList.firstChild);
+    }
+
+    // Show default message
+    if (todos.length === 0) {
+      const p = this.createElement("p");
+      p.textContent = "Nothing to do! Add a task?";
+      this.todoList.append(p);
+    } else {
+      todos.forEach(todo => {
+        const li = document.createElement("li");
+        li.id = todo.id;
+        li.innerHTML = `
+          <input type="checkbox" ${todo.complete ? "checked" : ""}>
+          <span contenteditable="true" class="editable">${
+            todo.complete ? `<s>${todo.text}</s>` : todo.text
+          }</span>
+          <button class="delete">Delete</button>  
+        `;
+
+        this.todoList.append(li);
+      });
+    }
   }
 }
 
